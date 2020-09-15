28735f7fe7eed36fbdad842c6f9a168d307b6420
Ofir Ben Shoham <Ofir408@gmail.com>
2020-09-15 08:45:39 +0300

    Changed db path to the working direcotry

--- Definitions.py
+++ Definitions.py
@@ -1,1 +1,4 @@
-DB_FILE_PATH = "C:/Users/Ofir/PycharmProjects/todo-list-backend/pythonsqlite.db"
+import os
+ROOT_DIR = os.path.abspath(os.curdir)
+DB_FILE_PATH = ROOT_DIR + "/pythonsqlite.db"
+print("DB_FILE_PATH = ", DB_FILE_PATH)
